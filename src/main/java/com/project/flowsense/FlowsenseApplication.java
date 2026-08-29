061a81daa3f232c5cbc0a7587912faa12187c8de
eduardofernandez197 <eduardofefe1970@gmail.com>
2026-08-29 15:06:24 -0300

    Subindo arquitetura MVC-Spring-Boot para Git

---
+++ src/main/java/com/project/flowsense/FlowsenseApplication.java
@@ -0,0 +1,13 @@
+package com.project.flowsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowsenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/flowsense/FlowsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flowsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
